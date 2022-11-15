912ef6ae9c46fa47e57607e0e9f3296931013aa0
Felipe Libório <felipeliborio@outlook.com>
2022-11-15 17:45:54 -0300

    criando aplicação para cadastro de interesses

---
+++ interests/src/test/java/br/edu/ufape/residencia/interests/InterestsApplicationTests.java
@@ -0,0 +1,11 @@
+package br.edu.ufape.residencia.interests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestsApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
